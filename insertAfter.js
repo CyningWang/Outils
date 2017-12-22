5195130508e50c5cbe233b2712c26eff3411abc0
Black-White <1652545601@qq.com>
2017-12-22 17:28:37 +0800

    Create insertAfter.js
    现有元素后插入一个新元素

---
+++ insertAfter.js
@@ -0,0 +1,13 @@
+/**
+ * @desc 在现有元素后插入一个新元素
+ * @param {Element} newElement
+ * @param {Element} targetElement
+ */
+function insertAfter(newElement, targetElement){
+    var parent = targetElement.parentNode;
+    if(parent.lastChild == targetElement){
+        parent.appendChild(newElement);
+    }else{
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
